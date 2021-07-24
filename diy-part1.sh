e4eee35b4cbcc6df6a68322e48fd622415b317fd
wanghuazho <69896092+wanghuazho@users.noreply.github.com>
2021-07-24 11:24:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git installN1 https://github.com/tuanqing/install-program' feeds.conf.default
+sed -i '$a src-git https://github.com/tuanqing/install-program package/install-program' feeds.conf.default
